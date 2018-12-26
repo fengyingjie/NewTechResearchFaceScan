b8b32046853c554a85698a1fb5b8eb30c816208f
fengyingjie <feng_yingjie1982@2163.com>
2018-12-26 16:57:52 +0800

    teste

--- app/src/main/java/bunkyo/fxs/china/gdc/fujitsu/com/newtechresearchfacescan/detector/FaceDetector.java
+++ app/src/main/java/bunkyo/fxs/china/gdc/fujitsu/com/newtechresearchfacescan/detector/FaceDetector.java
@@ -7,6 +7,13 @@ import android.os.HandlerThread;
 import android.os.Message;
 import android.util.Log;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
@@ -31,6 +38,7 @@ public class FaceDetector {
         public void run() {
             Log.d("FaceDetector","mPhotoQueue.size()：" + mPhotoQueue.size() + ",mThreadGroup.activeCount() :" + mThreadGroup.activeCount() );
             super.run();
+
             byte[] bitmap = mPhotoQueue.poll();
 
             while(bitmap != null) {
@@ -43,6 +51,7 @@ public class FaceDetector {
                         if(!"unknow".equals(name)) {
                             Log.d("FaceDetector", "find:" + name);
                         }
+                        postDataToServer(name,String.valueOf(System.currentTimeMillis()));
                     }
                 }
                 bitmap = mPhotoQueue.poll();
@@ -72,5 +81,56 @@ public class FaceDetector {
                 new DetectorThread().start();
             }
         }
+    }
+
+    protected void postDataToServer(String id , String time){
+
+        OutputStream os = null;
+        HttpURLConnection connection = null;
+        StringBuffer body = new StringBuffer("ACTION:FACEIN&ID:");
+        body.append(id);
+        body.append("&LOGINTIME:");
+        body.append(time);
+        byte[] data = body.toString().getBytes();
+        URL url = null;
+        try {
+            //获得URL对象
+            url = new URL("http://47.94.142.3/example/");
+            //获得HttpURLConnection对象
+            connection = (HttpURLConnection) url.openConnection();
+            // 设置请求方法为post
+            connection.setRequestMethod("POST");
+            //不使用缓存
+            connection.setUseCaches(false);
+            //设置超时时间
+            connection.setConnectTimeout(10000);
+            //设置读取超时时间
+            connection.setReadTimeout(10000);
+            //设置是否从httpUrlConnection读入,默认情况下是true;
+            connection.setDoInput(true);
+            //设置为true后才能写入参数
+            connection.setDoOutput(true);
+
+            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            connection.setRequestProperty("accept", "*/*");
+            connection.setRequestProperty("Content-Length", String.valueOf(data.length));
+            os = connection.getOutputStream();
+            os.write(data);
+            if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                Log.d("FaceDetector","HttpURLConnection.HTTP_OK" );
+            }else{
+                Log.d("FaceDetector","HttpURLConnection.HTTP_NG" );
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            connection.disconnect();
+            connection = null;
+        }
+
     }
 }
